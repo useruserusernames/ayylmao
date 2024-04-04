183b3503fedfffa82aa1649f54f79f00875b13d4
useruserusernames <152334142+useruserusernames@users.noreply.github.com>
2024-04-04 17:08:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         Genome Remover 1000
+// @version      1.00
+// @description  all my homies hate genome.
+// @author       you know who i am.
+// @downloadURL  https
+// @updateURL    https
+// @match        https://docs.google.com/spreadsheets/d/*
+// @match        https://docs.google.com/document/d/*
+// @match        https://docs.google.com/presentation/d/*
+// @match        https://docs.google.com/forms/d/*
+// @match        https://klickhealth.proofhq.com/review/*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// @noframes
+// ==/UserScript==
+
+
+function removeInjectedElements() {
+  var hubStyles = document.getElementById('hub-styles');
+  if (hubStyles) {
+    hubStyles.parentNode.removeChild(hubStyles);
+    console.log('Removed #hub-styles');
+  }
+
+  var genomeSidebar = document.getElementById('genome-sidebar');
+  if (genomeSidebar) {
+    genomeSidebar.parentNode.removeChild(genomeSidebar);
+    console.log('Removed #genome-sidebar');
+  }
+}
+
+
+window.onload = function() {
+  var intervalId = setInterval(removeInjectedElements, 500);
+
+  setTimeout(function() {
+    clearInterval(intervalId);
+    console.log('Stopped checking for elements to remove.');
+  }, 30000);
+};
